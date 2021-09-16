a1539732584295aa5542fc5e116243beff4dbf79
David-Abderhalden-1 <david.abderhalden@hotmail.com>
2021-09-16 14:46:04 +0200

    Project Structure Setup

--- src/com/github/ojanka/javachess/JavaChess.java
+++ src/com/github/ojanka/javachess/JavaChess.java
@@ -1,5 +1,10 @@
 package com.github.ojanka.javachess;
 
+import com.github.ojanka.javachess.game.Game;
+
 public class JavaChess {
 
+    public static void main(String[] args){
+        Game game = Game.getInstance();
+    }
 }
