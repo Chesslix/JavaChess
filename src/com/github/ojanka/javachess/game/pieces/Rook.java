b5267620d73fe7cc4d7fa696e910d6ddfc48040f
David-Abderhalden-1 <david.abderhalden@hotmail.com>
2021-09-23 16:26:46 +0200

    Rook validPositions finished
    Also added testing board setup and piece function which returns name

--- src/com/github/ojanka/javachess/game/pieces/Rook.java
+++ src/com/github/ojanka/javachess/game/pieces/Rook.java
@@ -5,6 +5,8 @@ import com.github.ojanka.javachess.game.Piece;
 import com.github.ojanka.javachess.util.ChessColor;
 import com.github.ojanka.javachess.util.Position;
 
+import java.util.ArrayList;
+
 public class Rook extends Piece {
 
 	public Rook(int startX, int startY, ChessColor color) {
@@ -14,22 +16,35 @@ public class Rook extends Piece {
 
 	@Override
 	public Position[] getValidPositions() {
-		long bitboardAllies = Game.getInstance().getBoard().getAlliesAsBitmap(this.getColor());
+		long bitboardAllies = Game.getInstance().getBoard().getAsBitmapByColor(this.getColor());
+		long bitboardEnemies = Game.getInstance().getBoard().getAsBitmapByColor(ChessColor.getOpposite(this.getColor()));
 		int cPos = getCurrentPosition().getY() * 8 + getCurrentPosition().getX();
+		ArrayList<Position> validPositions = new ArrayList<>();
 		// relative moves
-		int[] possibleMovesUp = {8, 16, 24, 32, 40, 48, 56};
-		int[] possibleMovesDown = {-8, -16, -24, -32, -40, -48, -56};
-		int[] possibleMovesLeft = {-1, -2, -3, -4, -5, -6, -7};
-		int[] possibleMovesRight = {1, 2, 3, 4, 5, 6, 7};
-		for(int move : possibleMovesUp){
-			// calc absolute move
-			// check if out of board	continue
-			// check if ally on pos		continue
-			// check if enemy on board  -> break
-
-			// -> add to validPositions
+		int[][] possibleMoves = {
+				{8, 16, 24, 32, 40, 48, 56}, 			// move up
+				{-8, -16, -24, -32, -40, -48, -56}, 	// move down
+				{-1, -2, -3, -4, -5, -6, -7}, 			// move left
+				{1, 2, 3, 4, 5, 6, 7},					// move right
+		};
+		for(int[] moves : possibleMoves){
+			for(int move : moves) {
+				// calc absolute move
+				int nPos = cPos + move;
+				// check if out of board	continue
+				if (nPos < 0 || nPos > 63) continue;
+				// check if ally on pos		continue
+				if (((bitboardAllies >> nPos) & 1) == 1) break;
+				// check if enemy on board  -> break
+				if (((bitboardEnemies >> nPos) & 1) == 1) {
+					validPositions.add(new Position(nPos % 8, nPos >> 3));
+					break;
+				}
+				// -> add to validPositions
+				validPositions.add(new Position(nPos % 8, nPos >> 3));
+			}
 		}
-		return null;
+		return validPositions.toArray(Position[]::new);
 	}
 
 	@Override
