7bda71c321e83fe091e0e3f64f995139e63bb864
David-Abderhalden-1 <david.abderhalden@hotmail.com>
2021-10-23 20:31:05 +0200

    Testing setup

--- src/com/github/ojanka/javachess/Development.java
+++ src/com/github/ojanka/javachess/Development.java
@@ -28,42 +28,50 @@ public class Development {
         Piece[] testGame = {
                 // WHITE:
                 // Figures =====================================
-                new Rook(2, 3, ChessColor.WHITE),		// left Rook
-                new Knight(1, 5, ChessColor.WHITE),		// left Knight
-                new King(3, 1, ChessColor.WHITE),		// 		King
-                new Queen(4, 2, ChessColor.WHITE),		// 		Queen
+                new Rook(0, 0, ChessColor.WHITE),		// left Rook
+                new Knight(1, 0, ChessColor.WHITE),		// left Knight
+                new Bishop(2, 0, ChessColor.WHITE),		// left Bishop
+                new King(3, 0, ChessColor.WHITE),		// 		King
+                new Queen(4, 0, ChessColor.WHITE),		// 		Queen
                 new Bishop(5, 0, ChessColor.WHITE),		// right Bishop
-                new Knight(5, 3, ChessColor.WHITE),		// right Knight
-                new Rook(6, 0, ChessColor.WHITE),		// right Rook
+                new Knight(6, 0, ChessColor.WHITE),		// right Knight
+                new Rook(7, 0, ChessColor.WHITE),		// right Rook
                 // Pawns =======================================
                 new Pawn(0, 1, ChessColor.WHITE),		// 1 Pawn
-                new Pawn(1, 4, ChessColor.WHITE),		// 2 Pawn
+                new Pawn(1, 1, ChessColor.WHITE),		// 2 Pawn
+                new Pawn(2, 1, ChessColor.WHITE),		// 3 Pawn
+                new Pawn(3, 5, ChessColor.WHITE),		// 4 Pawn
                 new Pawn(4, 1, ChessColor.WHITE),		// 5 Pawn
-                new Pawn(5, 2, ChessColor.WHITE),		// 6 Pawn
-                new Pawn(6, 2, ChessColor.WHITE),		// 7 Pawn
+                new Pawn(5, 1, ChessColor.WHITE),		// 6 Pawn
+                new Pawn(6, 1, ChessColor.WHITE),		// 7 Pawn
                 new Pawn(7, 1, ChessColor.WHITE),		// 8 Pawn
 
                 //BLACK:
                 // Figures =====================================
                 new Rook(0, 7, ChessColor.BLACK),		// left Rook
+                new Knight(1, 7, ChessColor.BLACK),		// left Knight
+                new Bishop(2, 7, ChessColor.BLACK),		// left Bishop
                 new King(3, 7, ChessColor.BLACK),		// 		King
                 new Queen(4, 7, ChessColor.BLACK),		// 		Queen
                 new Bishop(5, 7, ChessColor.BLACK),		// right Bishop
                 new Knight(6, 7, ChessColor.BLACK),		// right Knight
+                new Rook(7, 7, ChessColor.BLACK),		// right Rook
                 // Pawns =======================================
-                new Pawn(0, 2, ChessColor.BLACK),		// 1 Pawn
+                new Pawn(0, 6, ChessColor.BLACK),		// 1 Pawn
                 new Pawn(1, 6, ChessColor.BLACK),		// 2 Pawn
                 new Pawn(2, 6, ChessColor.BLACK),		// 3 Pawn
                 new Pawn(3, 6, ChessColor.BLACK),		// 4 Pawn
-                new Pawn(0, 1, ChessColor.WHITE),		// 4 Pawn
-                new Pawn(1, 2, ChessColor.BLACK),		// 4 Pawn
-                new Pawn(0, 3, ChessColor.BLACK),		// 4 Pawn
+                new Pawn(4, 6, ChessColor.BLACK),		// 5 Pawn
+                new Pawn(5, 6, ChessColor.BLACK),		// 6 Pawn
+                new Pawn(6, 6, ChessColor.BLACK),		// 7 Pawn
+                new Pawn(7, 6, ChessColor.BLACK),		// 8 Pawn
         };
         // CHECKMATE TEST
-        game.setupDefaultBoard();
+        game.setupBoard(testGame);
         long test = 0L;
         test += game.getBoard().getAllPossibleMovesBoard(ChessColor.BLACK);
 
+        game.consoleBoard();
     }
 
     public static void main(String[] args){
