11536da2902714ac3b2eb7f6b2c7063003145fd5
David Abderhalden <david.abderhalden@hotmail.com>
2021-11-05 23:03:44 +0100

    Fixed Queen Movement

--- src/com/github/ojanka/javachess/game/pieces/Queen.java
+++ src/com/github/ojanka/javachess/game/pieces/Queen.java
@@ -23,17 +23,30 @@ public class Queen extends Piece {
 		int[] possibleMoves = {1, 7, 8, 9, -1, -7, -8, -9};
 		for (int move : possibleMoves) {
 			for (int nPos = cPos + move; nPos < 64 && nPos > -1; nPos += move) {
+				// check if on left border
+				if(((nPos+1) % 8 == 0) && move == -1){
+					//validPositions.add(new Position(0, nPos >>> 3));
+					break;
+				}
+
+				// check if on right border
+				if((nPos % 8 == 0) && move == 1){
+					//validPositions.add(new Position(0, nPos >>> 3));
+					break;
+				}
+				// check if on border of field diagonal movement
+				if((nPos+1) % 8 == 0 || nPos % 8 == 0) {
+					// check if last field x axe was more than 1 difference (only for bishop)
+					if(Math.abs((nPos % 8)-((nPos-move) % 8)) > 1){
+						break;
+					}
+				}
 				// check if ally
 				if (((bitboardAllies >> nPos) & 1) == 1) {
 					break;
 				}
 				// check if enemy
 				if (((bitboardEnemies >> nPos) & 1) == 1) {
-					validPositions.add(new Position(nPos % 8, nPos >>> 3));
-					break;
-				}
-				// check if on boarder
-				if ((nPos + 1) % 8 == 0 || nPos % 8 == 0) {
 					validPositions.add(new Position(nPos % 8, nPos >>> 3));
 					break;
 				}
